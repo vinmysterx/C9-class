2362de3633ab7b4a6f72b80336c4560a1d12b05a
vinmysterx <94560455+vinmysterx@users.noreply.github.com>
2021-11-17 18:10:32 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var box; 
+function setup() {
+  createCanvas(400, 400);
+  box = createSprite(200,200,30,30);
+} 
+
+function draw() {
+  background(220);
+  if (keyIsDown(RIGHT_ARROW))
+   { 
+     box.position.x = box.position. x+5;
+     }
+     if (keyIsDown(LEFT_ARROW))
+     { 
+       box.position.x = box.position. x-5;
+       }
+  drawSprites();
+}
